d1922fdbffe1ac377e8e0639cb8113e0250a34d8
Akshaj05 <122291038+Akshaj05@users.noreply.github.com>
2025-04-05 16:48:21 +0530

    Added login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect, use } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { supabase } from "./CreateClient";
 
+// import Home from "./pages/home";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+
 const App = () => {
-  const [users, setUsers] = useState([]);
-  console.log(users);
+  // const [users, setUsers] = useState([]);
+  // console.log(users);
 
-  useEffect(() => {
-    fetchUserse();
-  }, []);
+  // useEffect(() => {
+  //   fetchUserse();
+  // }, []);
 
-  async function fetchUserse() {
-    const { data } = await supabase.from("users").select("*");
-    setUsers(data);
-    console.log(users);
-  }
-
-  return <div></div>;
+  // async function fetchUserse() {
+  //   const { data } = await supabase.from("users").select("*");
+  //   setUsers(data);
+  //   console.log(users);
+  // }
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/home" element={<Home />} /> */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+const home = () => {
+  return <div></div>;
+};
+
+export default home;
